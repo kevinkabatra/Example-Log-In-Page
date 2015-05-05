2063f6bee98973f9f6752c75d9d756d1122d080f
Kevin Kabatra <kevinkabatra@gmail.com>
2015-05-04 20:17:52 -0400

    Created sanitize_input.php

---
+++ sanitize_input.php
@@ -0,0 +1,54 @@
+<?php
+
+/* 
+ * Copyright (c) 2015, Kevin Kabatra <kevinkabatra@gmail.com>
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* 
+ * The code follows the Follow Field Naming Conventions from the 
+ * AOSP (Android Open Source Project) Code Style Guidelines for Contributors :
+ *     Non-public, non-static field names start with m.
+ *     Static field names start with s.
+ *     Other fields start with a lower case letter.
+ *     Public static final fields (constants) are ALL_CAPS_WITH_UNDERSCORES
+ * Hyperlink: (too long for one line)
+ *     http://source.android.com/source/code-style
+ *     .html#follow-field-naming-conventions
+ */
+
+    /**
+     * Calls trim() and stripslashes() on a string.
+     * <p>
+     * The trim() function strips unnecessary characters (extra space, tab, 
+     * newline) from the user input data. The stripslashes() function removes
+     * backslashes (\) from the user input data. 
+     * 
+     * @param {string} $mSubmittedInstream - String variable that was passed
+     * @returns {string}
+     */    
+    function sanitize_input($mSubmittedInstream) {
+        $mSubmittedInstream = trim($mSubmittedInstream);
+        $mSubmittedInstream = stripslashes($mSubmittedInstream);
+        return $mSubmittedInstream;
+    }
